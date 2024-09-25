db9b61a03044f43980c465102943d53821a6eb40
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-09-25 20:18:53 -0300

    added little stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './index.css'
 import Header from './components/Header'
 import Conocimientos from './components/Conocimientos'
-import Wave from './components/Wave'
 import ProyectLayout from './components/ProyectLayout'
+import starsBg from './assets/stars.svg'
 
 function App() {
   return (
     <>
       <div className='flex w-full flex-col items-center justify-start pb-20'>
+        <img
+          className='opacity-10 absolute bg-gradient-to-t from-white to-transparent'
+          src={starsBg}
+          alt='fondo de estrellas'
+        />
         <Header />
         <ProyectLayout />
         <Conocimientos />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SocialMedia from './SocialMedia'
 
+
 export default function Header() {
   return (
     <>
       <div className='relative py-20 justify-center items-center '>
         <header className='md:w-[40em] px-5'>
+          
           <div className=' animate-opacity w-full  flex flex-col justify-start items-start'>
             <h1 className='text-6xl font-extrabold '>Pablo Kuhn</h1>
             <p className='text-black'>
